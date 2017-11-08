7c87c05ee11a85996a6ca909b84b1e2b9a0c4151
liuyibox <liuyibox@outlook.com>
2017-11-07 22:00:53 -0600

    Update server.sh

--- server.sh
+++ server.sh
@@ -10,24 +10,24 @@ if [ $1 == "masterServer" ]; then
 	echo "master server has 4 master processes now:"
 	./fbsd -l 1 -p 6001 -h masterServer -m 1 -i 0 &
 	export pid1=$!
-	echo -n "get a thread "		
-	echo -n $pid1
-	echo " at 6001"		
+#	echo -n "get a thread "		
+#	echo -n $pid1
+#	echo " at 6001"		
 	./fbsd -p 6002 -h masterServer -m 1 -i 0 &
 	export pid2=$!	
-	echo -n "get a thread "		
-	echo -n $pid2
-	echo " at 6002"
+#	echo -n "get a thread "		
+#	echo -n $pid2
+#	echo " at 6002"
 	./fbsd -p 6003 -h masterServer -m 1 -i 0 &
 	export pid3=$!		
-	echo -n "get a thread "		
-	echo -n $pid3
-	echo " at 6003"
+#	echo -n "get a thread "		
+#	echo -n $pid3
+#	echo " at 6003"
 	./fbsd -c 1 -p 6004 -h masterServer -m 1 -i 0 &
 	export pid4=$!
-	echo -n "get a thread "		
-	echo -n $pid4
-	echo " at 6004"
+#	echo -n "get a thread "		
+#	echo -n $pid4
+#	echo " at 6004"
 #	wait $pid1 $pid2 $pid3 $pid4 &
 	control_c(){
 		kill $pid1
@@ -43,19 +43,19 @@ elif [ $1 == "server1" ]; then
 	echo "server1 has 3 processes now:"
 	./fbsd -l 1 -p 6005 -h server1 -i 1 &
 	export pid1=$!
-	echo -n "get a thread "		
-	echo -n $pid1
-	echo " at 6005"
+#	echo -n "get a thread "		
+#	echo -n $pid1
+#	echo " at 6005"
 	./fbsd -p 6006 -h server1 -i 1 &
 	export pid2=$!
-	echo -n "get a thread "		
-	echo -n $pid2
-	echo " at 6006"
+#	echo -n "get a thread "		
+#	echo -n $pid2
+#	echo " at 6006"
 	./fbsd -c 1 -p 6007 -h server1 -i 1&
 	export pid3=$!
-	echo -n "get a thread "		
-	echo -n $pid3
-	echo " at 6007"
+#	echo -n "get a thread "		
+#	echo -n $pid3
+#	echo " at 6007"
 #	wait $pid1 $pid2 $pid3
 	control_c(){
 		kill $pid1
@@ -70,19 +70,19 @@ else
 	echo "server2 has 3 processes now:"
 	./fbsd -l 1 -p 6008 -h server2 -i 2 &
 	export pid1=$!
-	echo -n "get a thread "		
-	echo -n $pid1
-	echo " at 6008"
+#	echo -n "get a thread "		
+#	echo -n $pid1
+#	echo " at 6008"
 	./fbsd -p 6009 -h server2 -i 2 &
 	export pid2=$!
-	echo -n "get a thread "		
-	echo -n $pid2
-	echo " at 6009"
+#	echo -n "get a thread "		
+#	echo -n $pid2
+#	echo " at 6009"
 	./fbsd -c 1 -p 6010 -h server2 -i 2 &
 	export pid3=$!
-	echo -n "get a thread "		
-	echo -n $pid3
-	echo " at 6010"
+#	echo -n "get a thread "		
+#	echo -n $pid3
+#	echo " at 6010"
 #	wait $pid1 $pid2 $pid3
 	control_c(){
 		kill $pid1
